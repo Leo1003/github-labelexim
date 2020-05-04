ff64df4b5c14f79249764d18175be06ea3d7e431
Leo <leo881003@gmail.com>
2020-05-05 01:59:48 +0800

    Add github repo parsing

--- src/api.rs
+++ src/api.rs
@@ -1,5 +1,22 @@
+use lazy_static::lazy_static;
 use serde::{Serialize, Deserialize};
+use regex::Regex;
 use rgb::RGB;
+
+const GH_OWNER_REGEX: &str = r"(?P<owner>[[:alnum:]][[:alnum:]\-]+[[:alnum:]])";
+const GH_REPO_REGEX: &str = r"(?P<repo>[[:alnum:]._\-]+?)";
+
+lazy_static! {
+    static ref GITHUB_REPO_HTTPS: Regex = {
+        Regex::new(&format!(r"^https://github\.com/{}/{}(.git)?$", GH_OWNER_REGEX, GH_REPO_REGEX)).unwrap()
+    };
+    static ref GITHUB_REPO_GIT: Regex = {
+        Regex::new(&format!(r"^git@github\.com:{}/{}(.git)?$", GH_OWNER_REGEX, GH_REPO_REGEX)).unwrap()
+    };
+    static ref GITHUB_REPO: Regex = {
+        Regex::new(&format!(r"^{}/{}$", GH_OWNER_REGEX, GH_REPO_REGEX)).unwrap()
+    };
+}
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Label {
@@ -8,6 +25,22 @@ pub struct Label {
     #[serde(with = "hex_color")]
     color: RGB<u8>,
 }
+
+pub fn parse_github_repo<'a> (s: &'a str) -> Option<(&'a str, &'a str)> {
+    #[inline]
+    fn _regcap<'a>(re: &Regex, s: &'a str) -> Option<(&'a str, &'a str)> {
+        re.captures(s).map(|cap| {
+            (cap.name("owner").unwrap().as_str(), cap.name("repo").unwrap().as_str())
+        })
+    }
+
+    _regcap(&GITHUB_REPO_HTTPS, s).or_else(|| {
+        _regcap(&GITHUB_REPO_GIT, s)
+    }).or_else(|| {
+        _regcap(&GITHUB_REPO, s)
+    })
+}
+
 
 mod hex_color {
     use rgb::RGB;
